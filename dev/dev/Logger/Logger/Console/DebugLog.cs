3492719824d0328e20761f6222d1a6980b787009
CountrySideEngineer <deep-sleeper@nifty.com>
2024-07-24 21:34:49 +0900

    Add DebugLog class. Add a new class DebugLog which has a method to output a message if the DEBUG constant is defined.

---
+++ dev/dev/Logger/Logger/Console/DebugLog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Logger.Console
+{
+	public class DebugLog : ALog
+	{
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public DebugLog() : base() { }
+
+		/// <summary>
+		/// Output log to debug console.
+		/// </summary>
+		/// <param name="message">Log message</param>
+		/// <remarks>
+		/// ATTENTION!!
+		/// This method only outputs a message if the DEBUG constant is defined.
+		/// (If the DEBUG constant is not defined, the message is not output.)
+		/// </remarks>
+		public override void Output(string message)
+		{
+#if DEBUG
+			Debug.WriteLine(message);
+#endif
+		}
+	}
+}
