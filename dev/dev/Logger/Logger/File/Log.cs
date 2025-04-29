0f98268a131a301a4ed241dcb92de239356b1c9d
CountrySideEngineer <deep-sleeper@nifty.com>
2025-04-29 14:53:18 +0900

    Update comment in File.Log class.

--- dev/dev/Logger/Logger/File/Log.cs
+++ dev/dev/Logger/Logger/File/Log.cs
@@ -8,8 +8,6 @@ namespace CS.Logger.File
 {
 	public class Log : ALog
 	{
-        protected StreamWriter _writer = null;
-
         protected List<string> _messageBuf = new List<string>();
 
         /// <summary>
@@ -111,8 +109,15 @@ namespace CS.Logger.File
 			string filePath = $@"{appDirPath}\{fileName}";
 
 			return filePath;
-		}
+        }
 
+        /// <summary>
+        /// Writes the accumulated log messages in the buffer to the log file.
+        /// </summary>
+        /// <remarks>
+        /// This method writes the current message buffer (_messageBuf) to the log file
+        /// using the Flush(IEnumerable<string>) method and clears the buffer after writing.
+        /// </remarks>
         protected virtual void Flush()
         {
             Flush(_messageBuf);
@@ -120,15 +125,24 @@ namespace CS.Logger.File
             _messageBuf.Clear();
         }
 
+        /// <summary>
+        /// Writes the provided collection of log messages to the log file.
+        /// </summary>
+        /// <param name="messages">A collection of log messages to be written to the log file.</param>
+        /// <remarks>
+        /// This method iterates through the provided messages and writes each one to the log file.
+        /// If an exception occurs during the write operation, it is caught and ignored, as there is no
+        /// mechanism to handle it. The StreamWriter is properly disposed of after the operation.
+        /// </remarks>
         protected virtual void Flush(IEnumerable<string> messages)
         {
             try
             {
-                using (_writer = new StreamWriter(FilePath, true))
+                using (var writer = new StreamWriter(FilePath, true))
                 {
                     foreach (var message in messages)
                     {
-                        _writer.WriteLine(message);
+                        writer.WriteLine(message);
                     }
                 }
             }
@@ -136,12 +150,19 @@ namespace CS.Logger.File
             {
                 //There is no way to handle the exception.
             }
-            finally
-            {
-                _writer = null;
-            }
         }
 
+        /// <summary>
+        /// Determines whether the message buffer has reached the threshold for flushing.
+        /// </summary>
+        /// <returns>
+        /// Returns <c>true</c> if the number of messages in the buffer exceeds the threshold (1000); 
+        /// otherwise, <c>false</c>.
+        /// </returns>
+        /// <remarks>
+        /// This method checks the size of the message buffer (_messageBuf) and decides if it should
+        /// be flushed to the log file. The threshold is currently set to 1000 messages.
+        /// </remarks>
         protected virtual bool SwitchBuf()
         {
             if (1000 < _messageBuf.Count)
@@ -154,6 +175,13 @@ namespace CS.Logger.File
             }
         }
 
+        /// <summary>
+        /// Releases resources used by the logger and ensures any remaining messages are flushed to the log file.
+        /// </summary>
+        /// <remarks>
+        /// This method overrides the base class's Dispose method. It flushes any remaining messages
+        /// in the buffer (_messageBuf) to the log file before releasing resources.
+        /// </remarks>
         public override void Dispose()
         {
             base.Dispose();
